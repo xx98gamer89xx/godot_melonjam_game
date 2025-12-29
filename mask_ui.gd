0fb8b56d83f870b0adcbf7728566408de0d2f34d
Xxgamer89xX <donarbustin123@gmail.com>
2025-12-29 11:11:41 +0100

    Mejoras en todo y reestructuración

---
+++ mask_ui.gd
@@ -0,0 +1,26 @@
+extends Control
+var player
+func _ready():
+	for node in get_parent().get_parent().get_children():
+		if node.name == "RigidBody2D":
+			print(node.name)
+			player = node
+
+func _process(delta):
+	if player != null:
+		if player.mask == 0 or player.mask == 1:
+			$TextureRect.texture = load("res://sprites/cara.png")
+		if player.mask == 2 or player.mask == 3:
+			$TextureRect.texture = load("res://sprites/mascara kitsune azul.png")
+		if player.mask == 4 or player.mask == 5:
+			$TextureRect.texture = load("res://sprites/mascara kitsune verde.png")
+		if player.mask == 6 or player.mask == 7:
+			$TextureRect.texture = load("res://sprites/mascara kitsune rosa.png")
+		if player.mask == 8 or player.mask == 9:
+			$TextureRect.texture = load("res://sprites/mascara oni azul.png")
+		if player.mask == 10 or player.mask == 11:
+			$TextureRect.texture = load("res://sprites/mascara oni verde.png")
+		if player.mask == 11 or player.mask == 13:
+			$TextureRect.texture = load("res://sprites/mascara oni rosa.png")
+		if player.mask == 14 or player.mask == 15:
+			$TextureRect.texture = load("res://sprites/mascara samurai.png")
---
+++ sprite_2d.gd
@@ -0,0 +1,35 @@
+extends Sprite2D
+func _process(delta):
+	var mask = get_parent().get_parent().mask
+	if mask == 0:
+		texture = load("res://sprites/prota sin mascara.png")
+	if mask == 1:
+		texture = load("res://sprites/prota sin mascara cuchillada.png")
+	if mask == 2:
+		texture = load("res://sprites/prota mascara kitsune ojos azules.png")
+	if mask == 3:
+		texture = load("res://sprites/prota mascara kitsune azul cuchillada.png")
+	if mask == 4:
+		texture = load("res://sprites/prota kitsune ojos verdes.png")
+	if mask == 5:
+		texture = load("res://sprites/prota mascara kitsune verde cuchillada.png")
+	if mask == 6:
+		texture = load("res://sprites/prota kitsune ojos rosas.png")
+	if mask == 7:
+		texture = load("res://sprites/prota mascara kitsune rosa cuchillada.png")
+	if mask == 8:
+		texture = load("res://sprites/prota mascara oni ojos azules.png")
+	if mask == 9:
+		texture = load("res://sprites/prota mascara oni azules cuchillada.png")
+	if mask == 10:
+		texture = load("res://sprites/prota mascara oni ojos verdes.png")
+	if mask == 11:
+		texture = load("res://sprites/prota mascara oni verdes cuchillada.png")
+	if mask == 12:
+		texture = load("res://sprites/prota mascara oni ojos rosas.png")
+	if mask == 13:
+		texture = load("res://sprites/prota mascara oni rosa cuchillada.png")
+	if mask == 14:
+		texture = load("res://sprites/prota mascara samurai.png")
+	if mask == 15:
+		texture = load("res://sprites/prota mascara samurai cuchillada.png")

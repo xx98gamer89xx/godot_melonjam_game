a8df63a959aa69f1b4d94d2523da1608b9e2b066
Xxgamer89xX <donarbustin123@gmail.com>
2025-12-29 17:44:42 +0100

    Mejoras en todo y reestructuración

---
+++ win_2.gd
@@ -0,0 +1,5 @@
+extends Area2D
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	z_index = 9
+	if body.is_in_group("objectives"):
+		get_tree().change_scene_to_file("res://you_win.tscn")
